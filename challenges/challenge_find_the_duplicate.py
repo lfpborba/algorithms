f2a05e074a3985e660163e8b131298024afcad30
lfpborba <luisfpb@icloud.com>
2023-02-10 01:29:28 -0300

    feat: implementing find_the_duplicate.py

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,22 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not is_valid_list(nums):
+        return False
+    nums_set = set()
+
+    for num in nums:
+        if num in nums_set:
+            return num
+        nums_set.add(num)
+        
+    return False
+
+
+def is_valid_list(nums):
+    if not nums or type(nums) != list or len(nums) < 2:
+        return False
+
+    for num in nums:
+        if type(num) != int or num < 1:
+            return False
+
+    return True
