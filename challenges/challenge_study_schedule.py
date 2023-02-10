7f85db28780af980d00e33554f04e58eaa03cfae
lfpborba <luisfpb@icloud.com>
2023-02-10 00:18:02 -0300

    feat: implementing study_schedule.py

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    student_qty = 0
+    if target_time is None:
+        return None
+    for element in permanence_period:
+        if all(isinstance(item, int) for item in element):
+            if element[0] <= target_time <= element[1]:
+                student_qty += 1
+        else:
+            return None
+
+    return student_qty
